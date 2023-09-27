61b1c8c8c63b239ecb05ff7c3a3f90c7a74a69d6
mlarrieta <mlarrieta@hotmail.com.ar>
2023-09-27 00:01:03 -0300

    primer commit

---
+++ Modelo/cliente.py
@@ -0,0 +1,13 @@
+class Cliente:
+    
+    def __init__(self, nombre, apellido, email, telefono, direccion):
+        
+        self.__nombre = nombre
+        self.__apellido = apellido
+        self.__email = email
+        self.__telefono = telefono
+        self.__direccion = direccion
+        
+        
+        
+        
---
+++ Modelo/usuario.py
@@ -0,0 +1,8 @@
+class Usuario:
+    
+    def __init__(self, name_usser, surname_usser, phone, address):
+        self.__name_usser = name_usser
+        self.__surname_usser = surname_usser
+        self.__phone = phone
+        self.__address = address
+        

b4f2dd39b72992415fa8557c0e36eae1da33a75d
mlarrieta <mlarrieta@hotmail.com.ar>
2023-09-27 13:47:43 -0300

    hola

---
+++ Modelo/articulo.py
@@ -0,0 +1,7 @@
+class Articulo:
+    
+    #stock_minimo: es la cantidad minima del articulo que el sistema permite para informar que debe reponer
+    #deberíamos tener un atributo reposicion??? para indicar la cantidad a reponer, o lo calculamos?? o pueder varias la reposicion?
+    
+    def __init__(self, cod_articulo, nombre, cantidad, precio, stock, stock_minimo):
+        pass

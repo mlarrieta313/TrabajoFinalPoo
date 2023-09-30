7f6540ce9f92b53811621697c7ed46bca222b847
mlarrieta <mlarrieta@hotmail.com.ar>
2023-09-30 17:32:20 -0300

    agregar clase venta y vista login

---
+++ Modelo/venta.py
@@ -0,0 +1,14 @@
+class Venta:
+    
+    def __init__(self, id, nro_venta, fecha_venta, id_cliente, nombre_cliente):
+        self.__id_venta = id
+        self.__nro_venta = nro_venta # correspode al nro de factura que se auto genera ej, N° xxxx-xxxxxxxx
+        self.__fecha_venta = fecha_venta
+        #self.__tipo_factura = tener en cuenta este atributo para diferenciar en el caso de tener herencia con presupuesto
+        self.__nombre_distribuidora = 'Clean Stock'
+        self.__domicilio_distribuidora = " "
+        self.__telefono_distribuidora = " "
+        self.__correo_electronico_dist = " "
+        self.__cuit_distribuidora = " "
+        self.__id_cliente = id_cliente
+        self.__nombre_cliente = nombre_cliente
